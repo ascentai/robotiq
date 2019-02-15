39c824bf38c0f172ea5cd41b630ce9973934acf8
Tom Stewart <thomas@ascent.ai>
2019-02-16 08:30:22 +0900

    added modbus node for 3f gripper, shortened pymodbus timeout

--- robotiq_3f_gripper_control/nodes/Robotiq3FGripperRtuNode.py
+++ robotiq_3f_gripper_control/nodes/Robotiq3FGripperRtuNode.py
@@ -36,37 +36,38 @@
 # Revision $Id$
 
 """@package docstring
-ROS node for controling a Robotiq 2F gripper using the Modbus RTU protocol.
+ROS node for controling a Robotiq 3F gripper gripper using the Modbus TCP protocol.
 
-The script takes as an argument the IP address of the gripper. It initializes a baseRobotiq2FGripper object and adds a comModbusTcp client to it. It then loops forever, reading the gripper status and updating its command. The gripper status is published on the 'Robotiq2FGripperRobotInput' topic using the 'Robotiq2FGripper_robot_input' msg type. The node subscribes to the 'Robotiq2FGripperRobotOutput' topic for new commands using the 'Robotiq2FGripper_robot_output' msg type. Examples are provided to control the gripper (Robotiq2FGripperSimpleController.py) and interpreting its status (Robotiq2FGripperStatusListener.py).
+The script takes as an argument the IP address of the gripper. It initializes a baseRobotiq3FGripper object and adds a comModbusTcp client to it. It then loops forever, reading the gripper status and updating its command.
+ The gripper status is published on the 'Robotiq3FGripperRobotInput' topic using the 'Robotiq3FGripper_robot_input' msg type. The node subscribes to the 'Robotiq3FGripperRobotOutput' topic for new commands using the 'Robotiq3FGripper_robot_output' msg type. Examples are provided to control the gripper (Robotiq3FGripperSimpleController.py) and interpreting its status (Robotiq3FGripperStatusListener.py).
 """
 
-import roslib; roslib.load_manifest('robotiq_2f_gripper_control')
-roslib.load_manifest('robotiq_modbus_rtu')
+import roslib; roslib.load_manifest('robotiq_3f_gripper_control')
+roslib.load_manifest('robotiq_modbus_tcp')
 import rospy
 from pymodbus.exceptions import ModbusIOException
-import robotiq_2f_gripper_control.baseRobotiq2FGripper
+import robotiq_3f_gripper_control.baseRobotiq3FGripper
 import robotiq_modbus_rtu.comModbusRtu
 import os, sys
-from robotiq_2f_gripper_control.msg import _Robotiq2FGripper_robot_input  as inputMsg
-from robotiq_2f_gripper_control.msg import _Robotiq2FGripper_robot_output as outputMsg
+from robotiq_3f_gripper_control.msg import _Robotiq3FGripper_robot_input  as inputMsg
+from robotiq_3f_gripper_control.msg import _Robotiq3FGripper_robot_output as outputMsg
 
 def mainLoop(device):
-    
-    #Gripper is a 2F with a RS485 connection
-    gripper = robotiq_2f_gripper_control.baseRobotiq2FGripper.robotiqbaseRobotiq2FGripper()
+
+    #Gripper is a 3F gripper with a RS485 connection
+    gripper = robotiq_3f_gripper_control.baseRobotiq3FGripper.robotiqbaseRobotiq3FGripper()
     gripper.client = robotiq_modbus_rtu.comModbusRtu.communication()
 
     #We connect to the address received as an argument
     gripper.client.connectToDevice(device)
 
-    rospy.init_node('robotiq2FGripper')
+    rospy.init_node('robotiq3FGripper')
 
-    #The Gripper status is published on the topic named 'Robotiq2FGripperRobotInput'
-    pub = rospy.Publisher('Robotiq2FGripperRobotInput', inputMsg.Robotiq2FGripper_robot_input)
+    #The Gripper status is published on the topic named 'Robotiq3FGripperRobotInput'
+    pub = rospy.Publisher('Robotiq3FGripperRobotInput', inputMsg.Robotiq3FGripper_robot_input, queue_size=1)
 
-    #The Gripper command is received from the topic named 'Robotiq2FGripperRobotOutput'
-    rospy.Subscriber('Robotiq2FGripperRobotOutput', outputMsg.Robotiq2FGripper_robot_output, gripper.refreshCommand)
+    #The Gripper command is received from the topic named 'Robotiq3FGripperRobotOutput'
+    rospy.Subscriber('Robotiq3FGripperRobotOutput', outputMsg.Robotiq3FGripper_robot_output, gripper.refreshCommand)
 
     #We loop
     while not rospy.is_shutdown():
@@ -86,3 +87,5 @@ if __name__ == '__main__':
     try:
         mainLoop(sys.argv[1])
     except rospy.ROSInterruptException: pass
+
+
--- robotiq_modbus_rtu/src/robotiq_modbus_rtu/comModbusRtu.py
+++ robotiq_modbus_rtu/src/robotiq_modbus_rtu/comModbusRtu.py
@@ -35,8 +35,6 @@
 #
 # Modifed from the orginal comModbusTcp by Kelsey Hawkins @ Georgia Tech
 
-
-
 """@package docstring
 Module comModbusRtu: defines a class which communicates with Robotiq Grippers using the Modbus RTU protocol. 
 
@@ -44,6 +42,8 @@ The module depends on pymodbus (http://code.google.com/p/pymodbus/) for the Modb
 """
 
 from pymodbus.client.sync import ModbusSerialClient
+from pymodbus.exceptions import ModbusIOException
+from pymodbus.register_write_message import WriteMultipleRegistersResponse
 from math import ceil
 
 class communication:	
@@ -52,8 +52,8 @@ class communication:
       self.client = None
       
    def connectToDevice(self, device):
-      """Connection to the client - the method takes the IP address (as a string, e.g. '192.168.1.11') as an argument."""
-      self.client = ModbusSerialClient(method='rtu',port=device,stopbits=1, bytesize=8, baudrate=115200, timeout=0.2)
+      """Connection to the client - the method takes the device name (as a string, e.g. '/dev/ttyUSB0') as an argument."""
+      self.client = ModbusSerialClient(method='rtu',port=device,stopbits=1, bytesize=8, baudrate=115200, timeout=0.05)
       if not self.client.connect():
           print "Unable to connect to %s" % device
           return False
@@ -83,9 +83,11 @@ class communication:
       """Sends a request to read, wait for the response and returns the Gripper status. The method gets the number of bytes to read as an argument"""
       numRegs = int(ceil(numBytes/2.0))
 
-      #To do!: Implement try/except 
       #Get status from the device
       response = self.client.read_holding_registers(0x07D0, numRegs, unit=0x0009)
+
+      if response is None or isinstance(response, WriteMultipleRegistersResponse):
+         raise ModbusIOException('No response from client')
 
       #Instantiate output as an empty list
       output = []
