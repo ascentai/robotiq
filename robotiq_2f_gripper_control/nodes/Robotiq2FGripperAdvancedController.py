cf796e2f235126b3e2ee1f4e92e501aaef2ad4a1
Tom Stewart <thomas@ascent.ai>
2019-03-11 17:56:18 +0900

    A foolish consistency is the hobgoblin of little minds

--- robotiq_2f_gripper_control/nodes/Robotiq2FGripperAdvancedController.py
+++ robotiq_2f_gripper_control/nodes/Robotiq2FGripperAdvancedController.py
@@ -7,21 +7,53 @@ from robotiq_common.msg import FingerJointCmd
 from robotiq_2f_gripper_control.msg import Robotiq2FGripper_robot_output, Robotiq2FGripper_robot_input
 
 
+class GripperJoint:
+    MIN_SPEED = 22.
+    MAX_SPEED = 110.
+    MIN_FORCE = 15.
+    MAX_FORCE = 60.
+
+    def __init__(self, name):
+        position = rospy.get_param(name + '/position', 1.0)
+        speed = rospy.get_param(name + '/speed', GripperJoint.MAX_SPEED)
+        force = rospy.get_param(name + '/force', GripperJoint.MIN_FORCE)
+
+        self.registers = [
+            int(np.clip(255 - position * 255, 0, 255)),
+            int(np.clip(speed - GripperJoint.MIN_SPEED / (GripperJoint.MAX_SPEED - GripperJoint.MIN_SPEED) * 255, 0, 255)),
+            int(np.clip(force - GripperJoint.MIN_FORCE / (GripperJoint.MAX_FORCE - GripperJoint.MIN_FORCE) * 255, 0, 255))
+        ]
+
+        self.position_cmd_pub = rospy.Publisher(name + '/position_cmd', Float32, queue_size=1)
+        self.position_pub = rospy.Publisher(name + '/position', Float32, queue_size=1)
+        self.current_pub = rospy.Publisher(name + '/current', Float32, queue_size=1)
+
+    def update_setpoint(self, sp_reg):
+        self.registers[0] = int(np.clip(255 - sp_reg * 255, 0, 255))
+
+    def publish_feedback(self, pos_reg, pos_cmd_reg, curr_reg):
+        self.position_pub.publish(Float32(data=1.-pos_reg/255.))
+        self.position_cmd_pub.publish(Float32(data=1.-pos_cmd_reg/255.))
+        self.current_pub.publish(Float32(data=curr_reg*0.01))
+
+    def get_registers(self):
+        return self.registers
+
+
 class TwoFingerGripperController:
     def __init__(self):
-        self.joint = FingerJoint('joint')
+        self.joint = GripperJoint('joint')
+
         self.output_pub = rospy.Publisher('Robotiq2FGripperRobotOutput', Robotiq2FGripper_robot_output, queue_size=1)
 
-        msg = rospy.wait_for_message('Robotiq2FGripperRobotInput', Robotiq2FGripper_robot_input)
-
-        if msg.gSTA != 3:
+        if rospy.wait_for_message('Robotiq2FGripperRobotInput', Robotiq2FGripper_robot_input).gSTA != 3:
             self.output_pub.publish(Robotiq2FGripper_robot_output())
             rospy.sleep(0.1)
 
         self.output_pub.publish(self.make_cmd())
 
         self.input_sub = rospy.Subscriber('Robotiq2FGripperRobotInput', Robotiq2FGripper_robot_input, self.input_cb)
-        self.update_sub = rospy.Subscriber('FingerJointCmds', FingerJointCmd, self.update_cb)
+        self.update_sub = rospy.Subscriber('GripperCmd', Float32, self.update_cb)
 
     def make_cmd(self):
         cmd = Robotiq2FGripper_robot_output(rACT=1, rGTO=1)
@@ -29,10 +61,7 @@ class TwoFingerGripperController:
         return cmd
 
     def update_cb(self, msg):
-        for name, setpoint in zip(msg.finger_names, msg.setpoints):
-            if name == 'joint':
-                self.joint.update_setpoint(setpoint)
-
+        self.joint.update_setpoint(msg.data)
         self.output_pub.publish(self.make_cmd())
 
     def input_cb(self, msg):
--- robotiq_3f_gripper_control/nodes/Robotiq3FGripperAdvancedController.py
+++ robotiq_3f_gripper_control/nodes/Robotiq3FGripperAdvancedController.py
@@ -1,50 +1,81 @@
 #!/usr/bin/env python
 import rospy
 import numpy as np
-import copy
 from std_msgs.msg import Float32
-from std_srvs.srv import Trigger
-from robotiq_common.FingerJoint import FingerJoint
-from robotiq_common.msg import FingerJointCmd
-from robotiq_3f_gripper_control.msg import Robotiq3FGripper_robot_input, Robotiq3FGripper_robot_output
+from robotiq_3f_gripper_control.msg import Robotiq3FGripper_robot_input, Robotiq3FGripper_robot_output, Robotiq3fGripperCmd
+
+
+class GripperJoint:
+    MIN_SPEED = 22.
+    MAX_SPEED = 110.
+    MIN_FORCE = 15.
+    MAX_FORCE = 60.
+
+    def __init__(self, name):
+        position = rospy.get_param(name + '/position', 1.0)
+        speed = rospy.get_param(name + '/speed', GripperJoint.MAX_SPEED)
+        force = rospy.get_param(name + '/force', GripperJoint.MIN_FORCE)
+
+        self.registers = [
+            int(np.clip(255 - position * 255, 0, 255)),
+            int(np.clip(speed - GripperJoint.MIN_SPEED / (GripperJoint.MAX_SPEED - GripperJoint.MIN_SPEED) * 255, 0, 255)),
+            int(np.clip(force - GripperJoint.MIN_FORCE / (GripperJoint.MAX_FORCE - GripperJoint.MIN_FORCE) * 255, 0, 255))
+        ]
+
+        self.position_cmd_pub = rospy.Publisher(name + '/position_cmd', Float32, queue_size=1)
+        self.position_pub = rospy.Publisher(name + '/position', Float32, queue_size=1)
+        self.current_pub = rospy.Publisher(name + '/current', Float32, queue_size=1)
+
+    def update_setpoint(self, sp_reg):
+        self.registers[0] = int(np.clip(255 - sp_reg * 255, 0, 255))
+
+    def publish_feedback(self, pos_reg, pos_cmd_reg, curr_reg):
+        self.position_pub.publish(Float32(data=1.-pos_reg/255.))
+        self.position_cmd_pub.publish(Float32(data=1.-pos_cmd_reg/255.))
+        self.current_pub.publish(Float32(data=curr_reg*0.01))
+
+    def get_registers(self):
+        return self.registers
 
 
 class ThreeFingerGripperController:
     def __init__(self):
-        self.joints = {name : FingerJoint(name) for name in ['fingerA','fingerB','fingerC','scissor']}
+        self.fingerA = GripperJoint('fingerA')
+        self.fingerB = GripperJoint('fingerB')
+        self.fingerC = GripperJoint('fingerC')
+        self.scissor = GripperJoint('scissor')
+
         self.output_pub = rospy.Publisher('Robotiq3FGripperRobotOutput', Robotiq3FGripper_robot_output, queue_size=1)
-
-        msg = rospy.wait_for_message('Robotiq3FGripperRobotInput', Robotiq3FGripper_robot_input)
-
-        if msg.gSTA != 3:
+        
+        if rospy.wait_for_message('Robotiq3FGripperRobotInput', Robotiq3FGripper_robot_input).gSTA != 3:
             self.output_pub.publish(Robotiq3FGripper_robot_output())
             rospy.sleep(0.1)
 
         self.output_pub.publish(self.make_cmd())
 
         self.input_sub = rospy.Subscriber('Robotiq3FGripperRobotInput', Robotiq3FGripper_robot_input, self.input_cb)
-        self.update_sub = rospy.Subscriber('FingerJointCmds', FingerJointCmd, self.update_cb)
+        self.update_sub = rospy.Subscriber('GripperCmd', Robotiq3fGripperCmd, self.update_cb)
 
     def make_cmd(self):
         cmd = Robotiq3FGripper_robot_output(rACT=1, rGTO=1, rICF=1, rICS=1)
-        cmd.rPRA, cmd.rSPA, cmd.rFRA = self.joints['fingerA'].get_registers()
-        cmd.rPRB, cmd.rSPB, cmd.rFRB = self.joints['fingerB'].get_registers()
-        cmd.rPRC, cmd.rSPC, cmd.rFRC = self.joints['fingerC'].get_registers()
-        cmd.rPRS, cmd.rSPS, cmd.rFRS = self.joints['scissor'].get_registers()
+        cmd.rPRA, cmd.rSPA, cmd.rFRA = self.fingerA.get_registers()
+        cmd.rPRB, cmd.rSPB, cmd.rFRB = self.fingerB.get_registers()
+        cmd.rPRC, cmd.rSPC, cmd.rFRC = self.fingerC.get_registers()
+        cmd.rPRS, cmd.rSPS, cmd.rFRS = self.scissor.get_registers()
         return cmd
 
     def update_cb(self, msg):
-        for name, setpoint in zip(msg.finger_names, msg.setpoints):
-            if name in self.joints:
-                self.joints[name].update_setpoint(setpoint)
-
+        self.fingerA.update_setpoint(msg.fingerA)
+        self.fingerB.update_setpoint(msg.fingerB)
+        self.fingerC.update_setpoint(msg.fingerC)
+        self.scissor.update_setpoint(msg.scissor)
         self.output_pub.publish(self.make_cmd())
 
     def input_cb(self, msg):
-        self.joints['fingerA'].publish_feedback(msg.gPOA, msg.gPRA, msg.gCUA)
-        self.joints['fingerB'].publish_feedback(msg.gPOB, msg.gPRB, msg.gCUB)
-        self.joints['fingerC'].publish_feedback(msg.gPOC, msg.gPRC, msg.gCUC)
-        self.joints['scissor'].publish_feedback(msg.gPOS, msg.gPRS, msg.gCUS)
+        self.fingerA.publish_feedback(msg.gPOA, msg.gPRA, msg.gCUA)
+        self.fingerB.publish_feedback(msg.gPOB, msg.gPRB, msg.gCUB)
+        self.fingerC.publish_feedback(msg.gPOC, msg.gPRC, msg.gCUC)
+        self.scissor.publish_feedback(msg.gPOS, msg.gPRS, msg.gCUS)
 
 
 if __name__ == '__main__':
